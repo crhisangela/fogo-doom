6f823be041e1a436f9a4b9201c80cad7ad8fe20f
Crhis Ferreira <ferreiracrhis@gmail.com>
2021-10-02 09:14:14 -0300

    18:05

--- Fogo/fire.js
+++ Fogo/fire.js
@@ -1,11 +1,14 @@
 const firePixelsArray = []
-const fireWidth = 2
-const fireHeight = 3
+const fireWidth = 10
+const fireHeight = 10
 
 
 function start() {
     createFireDataStructure()
-    console.log(firePixelsArray)
+    createFireSource()
+    renderFire()
+
+    setInterval(calculateFirePropagation, 1000)
 }
 
 function createFireDataStructure(){
@@ -15,21 +18,42 @@ function createFireDataStructure(){
     }
 }
 function calculateFirePropagation(){
+    for (let column = 0; column < fireWidth; column++){
+        for (let row = 0; row < fireHeight; row++){
+            const pixelIndex = column + (fireWidth *row)
+            console.log(pixelIndex)
+        }
     }
+}
 function renderFire(){
     let html = '<table cellpadding=0 cellspacing=0>'
     for (let row = 0; row < fireHeight; row++) {
         html += '<tr>'
 
         for (let column = 0; column <fireWidth; column++) {
-            html += '<td>'
+            const pixelIndex = column + (fireWidth * row)
+            const fireIntensity = firePixelsArray(pixelIndex)
 
+            html += '<td>'
+            html += `<div class="pixel-index">${pixelIndex}</div>`
+            html += fireIntensity
             html += '</td>'
         }
 
         html += '</tr>'
     }
     html =+ '</table>'
+
+    document.querySelector('#fireCanvas').innerHTML = html
+}
+
+function createFireSource() {
+    for (let column = 0; column <= fireWidth; column++) {
+        const overflowPixelIndex = fireWidth * fireHeight
+        const pixelIndex = (overflowPixelIndex - fireWidth) +column
+
+        firePixelsArray[pixelIndex] = 36
+    }
 }
 
 start()
